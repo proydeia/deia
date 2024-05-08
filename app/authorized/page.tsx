dac0c089e03e261a137e2b03a8d3a811ddd07909
DE BARBARA FADOL BENJAMIN <48110679@ort>
2024-05-08 15:17:50 -0300

    arreglando el login

--- app/authorized/page.tsx
+++ app/authorized/page.tsx
@@ -1,13 +1,16 @@
 import { auth } from "@/auth";
 import Logout from "./logOut_button";
+import { getUserList } from "../lib/db/schema";
 export default async function AuthorizedPage() {
-    console.log(await auth());
+    console.log(await auth())
+    const users = await getUserList();
 
     return(
         <>
         <main>
             <div>
                 <h1>Authorized page</h1>
+                <h2>{JSON.stringify(users)}</h2>
                 <Logout/>
             </div>
         </main>
--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-//import { logIn, hash } from './app/lib/db/schema';
+import { login } from './app/lib/db/schema';
 import { User } from 'next-auth';
 
  
@@ -10,18 +10,20 @@ export const { auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
       async authorize(credentials) {
-        //   const username = credentials.user as string;
-        //   const password = await hash(credentials.password as string);
 
-        //   console.log(password);
-  
-        //   const inputData:{name:string, password:string} = {name: username, password: password };
-          
-        //   const user = await logIn(inputData);
-          
-        //   return user as User;
-        return credentials as User//{name:"jojo", email:"jojo@gmail.com"} as User;
-    },
+        const username = credentials.user as string;
+        const password = credentials.password as string;
+        
+        const inputData:{name:string, password:string} = {name: username, password: password};
+        
+        const user = await login(inputData);
+        if(!user)
+        {
+          return null;
+        }   
+
+        return user;
+      },
     }),
   ],
 });
