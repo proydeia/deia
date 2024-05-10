968e3a30fa7a01ace95808eea82e19ede1367860
JulietaGT <julietaguerson@gmail.com>
2024-05-10 14:21:53 -0300

    arrgle el layout del medico

--- app/authorized/page.tsx
+++ app/authorized/page.tsx
@@ -4,10 +4,10 @@ export default async function AuthorizedPage() {
     return(
         <>
         <main>
-            <div className="relative flex justify-center flex-col items-center">
+            <div className="relative grid justify-center grid-cols-12 gap-4">
         <NavbarA/>
-                <h1>Mostrar lista de pacientes</h1>
-                <div className="bg-teal-200 w-10/12 items-center h-screen">Esto contendra la lista de pacientes </div>
+                
+                <div className="bg-teal-200 w-10/12 items-center h-screen col-start-3 col-end-12">Esto contendra la lista de pacientes </div>
             </div>
         </main>
         </>
