a1a288dea32ed40c785a9ff878e67a9d947f629c
JulietaGT <julietaguerson@gmail.com>
2024-05-15 10:52:14 -0300

    prueba new branch

--- app/authorized/paciente/page.tsx
+++ app/authorized/paciente/page.tsx
@@ -4,7 +4,7 @@ export default function ingresoPaciente() {
         <>
             <NavbarA />
             <div className="flex justify-center items-center h-screen">
-                <div className=" w-1/2 h-1/2 bg-primary rounded-lg">ola</div>
+                <div className=" w-1/2 h-1/2 bg-primary rounded-lg">hello</div>
 
             </div>
         </>
