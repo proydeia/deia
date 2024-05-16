121f486731de400192bec712ff24539484e2f8be
NeoMakesGames <61994917+NeoMakesGames@users.noreply.github.com>
2024-05-16 13:35:33 -0300

    Mejoras sistema convercion
    Nuevas formas de ingresar paths, fin de sistema de loop (ahora settea en el json de datadump cuando algo ya fue agregado previamente a los jsons de entrenamiento)

--- ds/convert.py
+++ ds/convert.py
@@ -7,16 +7,25 @@ def json_to_obj(path):
     with open(path) as f:
         return json.load(f)
     
+def obj_to_json(obj, path):
+    with open(path, 'w') as f:
+        json.dump(obj, f)
+    
 dotenv.load_dotenv('ds/.env')
 
-obj = json_to_obj('ds/example.json')
-obsJson = json_to_obj('ds/obs.json')
-resJson = json_to_obj('ds/res.json')
+nombreDumpDB = 'datadump'
+obj = json_to_obj(f'ds/{nombreDumpDB}.json')
+nombreJsonObstruccion = 'obs'
+obsJson = json_to_obj(f'ds/{nombreJsonObstruccion}.json')
+nombreJsonRestriccion = 'res'
+resJson = json_to_obj(f'ds/{nombreJsonRestriccion}.json')
 genai.configure(api_key=os.environ['API_KEY'])
 model = genai.GenerativeModel()
 #response = model.generate_content('I will now send you a text message with extra data about someone. I want you to send me, in messages separated by a new line, the format specified in each of the following factors: smoking (a number 0 (if they dont) or 1 (if they do)), age (a whole number), sex (a number 0 (male) or 1 (female)), height (a whole number), weight (a floating number). Just send me the data in parenthesis and nothing more. If you understand respond "Ok.".')
 #print(response.text)
 starting_question = 'I will now send you a text message with extra data about someone. I want you to send me, in messages separated by a new line, the format specified in each of the following factors: smoking (a number 0 (if they dont) or 1 (if they do)), age (a whole number), sex (a number 0 (male) or 1 (female)), height (a whole number), weight (a floating number). Just send me the data in parenthesis and nothing more. Message:\n\n'
+
+dumpAHacer = False
 
 for k, v in obj.items():
 
@@ -26,23 +35,23 @@ for k, v in obj.items():
         keys.append(k2)
         vals.append(v2)
 
-    message = vals[11]
-    valsExtra = [-1 for _ in range(5)]
-    print("Processing AI...")
-    response = model.generate_content(starting_question + message).text
-    data = response.split('\n')
-    correctData = []
-    for d in data:
-        try:
-            correctData.append(int(d.replace('(', '').replace(')', '')))
-        except:
-            correctData.append(float(d.replace('(', '').replace(')', '')))
-    print(correctData)
-
     vObs = {}
     vRes = {}
 
     if vals[10] == 0:
+        dumpAHacer = True
+        message = vals[11]
+        valsExtra = [-1 for _ in range(5)]
+        print("Processing AI...")
+        response = model.generate_content(starting_question + message).text
+        data = response.split('\n')
+        correctData = []
+        for d in data:
+            try:
+                correctData.append(int(d.replace('(', '').replace(')', '')))
+            except:
+                correctData.append(float(d.replace('(', '').replace(')', '')))
+        print(correctData)
 
         if vals[6] == 1:
             vObs["obstruction"] = v["correctionobsmed"]
@@ -64,11 +73,13 @@ for k, v in obj.items():
                 vRes[keys[12 + i]] = correctData[i]
             resJson[k] = vRes
 
-# Save the jsons
-with open('ds/obs.json', 'w') as f:
-    json.dump(obsJson, f)
+        obj[k]["enjson"] = 1
+    else:
+        print(f'{k.capitalize()} ya chequeado!')
 
-with open('ds/res.json', 'w') as f:
-    json.dump(resJson, f)
+if dumpAHacer:
+    obj_to_json(obj, f'ds/{nombreDumpDB}.json')
+    obj_to_json(resJson, f'ds/{nombreJsonRestriccion}.json')
+    obj_to_json(obsJson, f'ds/{nombreJsonObstruccion}.json')
 
-print("Done!")
+    print("Dump finalizado!")
