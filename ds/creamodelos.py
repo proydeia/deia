7afbd2acb32f637cba16e5e0aed2cdf24eb2fae8
NeoMakesGames <61994917+NeoMakesGames@users.noreply.github.com>
2024-06-11 11:57:31 -0300

    Made validation of models. Have to change res model to a logistic regression with the values remapped from (0;5) to (0;1)

--- ds/creamodelos.py
+++ ds/creamodelos.py
@@ -1,8 +1,21 @@
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import r2_score, accuracy_score
 import pickle
 import json
+import pandas as pd
+import os
 
 minimum = 10
+
+def CompareLinearModels(model1, model2, valDataX, valDataY):
+    score1 = r2_score(valDataY, model1.predict(valDataX))
+    score2 = r2_score(valDataY, model2.predict(valDataX))
+    return score1, score2
+
+def CompareLogisticModels(model1, model2, valDataX, valDataY):
+    score1 = accuracy_score(valDataY, model1.predict(valDataX))
+    score2 = accuracy_score(valDataY, model2.predict(valDataX))
+    return score1, score2
 
 obs = None
 with open('ds/obs.json') as f:
@@ -22,13 +35,29 @@ if obsEntries >= minimum:
         xObs.append(list(vals.values()))
         yObs.append(v['obstruction'])
 
+    xObsTrain = xObs[:int(0.8 * len(xObs))]
+    yObsTrain = yObs[:int(0.8 * len(yObs))]
+    xObsVal = xObs[int(0.8 * len(xObs)):]
+    yObsVal = yObs[int(0.8 * len(yObs)):]
+
     modelObs = LinearRegression()
-    modelObs.fit(xObs, yObs)
+    modelObs.fit(xObsTrain, yObsTrain)
 
-    with open('ds/modelObs.pkl', 'wb') as f:
-        pickle.dump(modelObs, f)
-
-    print('Models created and saved.')
+    if os.path.exists('ds/modelObs.pkl'):
+        with open('ds/modelObs.pkl', 'rb') as f:
+            modelObsOld = pickle.load(f)
+            score1, score2 = CompareLinearModels(modelObs, modelObsOld, xObsVal, yObsVal)
+            print(score1, score2)
+            if score1 > score2:
+                modelObs = modelObsOld
+                with open('ds/modelObs.pkl', 'wb') as j:
+                    pickle.dump(modelObs, j)
+            else:
+                print('Old model is better or equal.')
+    else:
+        with open('ds/modelObs.pkl', 'wb') as j:
+            pickle.dump(modelObs, j)
+        print('Models created and saved.')
 else:
     print('Too few entries.')
 
@@ -47,12 +76,28 @@ if resEntries >= minimum:
         xRes.append(list(vals.values()))
         yRes.append(v['restriction'])
 
+    xResTrain = xRes[:int(0.8 * len(xRes))]
+    yResTrain = yRes[:int(0.8 * len(yRes))]
+    xResval = xRes[int(0.8 * len(xRes)):]
+    yResval = yRes[int(0.8 * len(yRes)):]
+
     modelRes = LogisticRegression(class_weight="balanced")
-    modelRes.fit(xRes, yRes)
+    modelRes.fit(xResTrain, yResTrain)
 
-    with open('ds/modelRes.pkl', 'wb') as f:
-        pickle.dump(modelRes, f)
-
-    print('Models created and saved.')
+    if os.path.exists('ds/modelRes.pkl'):
+        with open('ds/modelRes.pkl', 'rb') as f:
+            modelResOld = pickle.load(f)
+            score1, score2 = CompareLogisticModels(modelRes, modelResOld, xResval, yResval)
+            print(score1, score2)
+            if score1 > score2:
+                modelRes = modelResOld
+                with open('ds/modelRes.pkl', 'wb') as j:
+                    pickle.dump(modelRes, j)
+            else:
+                print('Old model is better or equal.')
+    else:
+        with open('ds/modelRes.pkl', 'wb') as j:
+            pickle.dump(modelRes, j)
+        print('Models created and saved.')
 else:
     print('Too few entries.')
