99319ca07fead4e2c74f5e4cf0b8242c93692790
NeoMakesGames <61994917+NeoMakesGames@users.noreply.github.com>
2024-06-19 14:19:11 -0300

    RED NEURONAL WACHOOOO

--- ds/creamodelos.py
+++ ds/creamodelos.py
@@ -1,15 +1,16 @@
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.metrics import r2_score, accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score, mean_squared_error, accuracy_score
 import pickle
 import json
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 import os
 
-minimum = 10
-
-def CompareLinearModels(model1, model2, valDataX, valDataY):
-    score1 = r2_score(valDataY, model1.predict(valDataX))
-    score2 = r2_score(valDataY, model2.predict(valDataX))
+def CompareKerasModels(model1, model2, valDataX, valDataY):
+    score1 = mean_squared_error(valDataY, model1.predict(valDataX))
+    score2 = mean_squared_error(valDataY, model2.predict(valDataX))
     return score1, score2
 
 def CompareLogisticModels(model1, model2, valDataX, valDataY):
@@ -25,85 +26,76 @@ res = None
 with open('ds/res.json') as f:
     res = json.load(f)
 
-obsEntries = len(obs)
-if obsEntries >= minimum:
-    xObs = []
-    yObs = []
-    for k, v in obs.items():
-        vals = v.copy()
-        del vals['obstruction']
-#        del vals['fuma']
-#        del vals['edad']
-#        del vals['sexo']
-#        del vals['altura']
-#        del vals['peso']
-        xObs.append(list(vals.values()))
-        yObs.append(v['obstruction'])
+### Obstruction model
 
-    xObsTrain = xObs[:int(0.8 * len(xObs))]
-    yObsTrain = yObs[:int(0.8 * len(yObs))]
-    xObsVal = xObs[int(0.8 * len(xObs)):]
-    yObsVal = yObs[int(0.8 * len(yObs)):]
+xObs = []
+yObs = []
+for k, v in obs.items():
+    vals = v.copy()
+    del vals['obstruction']
+    xObs.append(list(vals.values()))
+    yObs.append(v['obstruction'] / 5)
 
-    modelObs = LinearRegression()
-    modelObs.fit(xObsTrain, yObsTrain)
+xObsTrain = xObs[:int(0.8 * len(xObs))]
+yObsTrain = yObs[:int(0.8 * len(yObs))]
+xObsVal = xObs[int(0.8 * len(xObs)):]
+yObsVal = yObs[int(0.8 * len(yObs)):]
 
-    if os.path.exists('ds/modelObs.pkl'):
-        with open('ds/modelObs.pkl', 'rb') as f:
-            modelObsOld = pickle.load(f)
-            score1, score2 = CompareLinearModels(modelObs, modelObsOld, xObsVal, yObsVal)
-            print(score1, score2)
-            if score1 > score2:
-                modelObs = modelObsOld
-                with open('ds/modelObs.pkl', 'wb') as j:
-                    pickle.dump(modelObs, j)
-            else:
-                print('Old model is better or equal.')
-    else:
-        print(r2_score(yObsVal, modelObs.predict(xObsVal)))
-        with open('ds/modelObs.pkl', 'wb') as j:
-            pickle.dump(modelObs, j)
-        print('Models created and saved.')
+modelObs = Sequential()
+modelObs.add(Dense(64, input_dim=9, activation='relu'))
+modelObs.add(Dense(32, activation='relu'))
+modelObs.add(Dense(16, activation='relu'))
+modelObs.add(Dense(1, activation='sigmoid'))
+modelObs.compile(loss='mean_squared_error', optimizer=Adam())
+modelObs.fit(pd.DataFrame(xObsTrain), pd.DataFrame(yObsTrain), epochs=20, batch_size=16)
+
+if os.path.exists('ds/modelObs.pkl'):
+    with open('ds/modelObs.pkl', 'rb') as f:
+        modelObsOld = pickle.load(f)
+        score1, score2 = CompareKerasModels(modelObs, modelObsOld, xObsVal, yObsVal)
+        print(score1, score2)
+        if score1 < score2:
+            print('New model is better.')
+            with open('ds/modelObs.pkl', 'wb') as j:
+                pickle.dump(modelObs, j)
+        else:
+            print('Old model is better or equal.')
 else:
-    print('Too few entries.')
+    print(r2_score(yObsVal, modelObs.predict(xObsVal)))
+    with open('ds/modelObs.pkl', 'wb') as j:
+        pickle.dump(modelObs, j)
+    print('Models created and saved.')
 
-resEntries = len(res)
-if resEntries >= minimum:
-    xRes = []
-    yRes = []
-    for k, v in res.items():
-        vals = v.copy()
-        del vals['restriction']
-#        del vals['fuma']
-#        del vals['edad']
-#        del vals['sexo']
-#        del vals['altura']
-#        del vals['peso']
-        xRes.append(list(vals.values()))
-        yRes.append(v['restriction'])
+### Restriction model
 
-    xResTrain = xRes[:int(0.8 * len(xRes))]
-    yResTrain = yRes[:int(0.8 * len(yRes))]
-    xResval = xRes[int(0.8 * len(xRes)):]
-    yResval = yRes[int(0.8 * len(yRes)):]
+xRes = []
+yRes = []
+for k, v in res.items():
+    vals = v.copy()
+    del vals['restriction']
+    xRes.append(list(vals.values()))
+    yRes.append(v['restriction'])
 
-    modelRes = LogisticRegression(class_weight="balanced")
-    modelRes.fit(xResTrain, yResTrain)
+xResTrain = xRes[:int(0.8 * len(xRes))]
+yResTrain = yRes[:int(0.8 * len(yRes))]
+xResval = xRes[int(0.8 * len(xRes)):]
+yResval = yRes[int(0.8 * len(yRes)):]
 
-    if os.path.exists('ds/modelRes.pkl'):
-        with open('ds/modelRes.pkl', 'rb') as f:
-            modelResOld = pickle.load(f)
-            score1, score2 = CompareLogisticModels(modelRes, modelResOld, xResval, yResval)
-            print(score1, score2)
-            if score1 > score2:
-                modelRes = modelResOld
-                with open('ds/modelRes.pkl', 'wb') as j:
-                    pickle.dump(modelRes, j)
-            else:
-                print('Old model is better or equal.')
-    else:
-        with open('ds/modelRes.pkl', 'wb') as j:
-            pickle.dump(modelRes, j)
-        print('Models created and saved.')
+modelRes = LogisticRegression(class_weight="balanced")
+modelRes.fit(xResTrain, yResTrain)
+
+if os.path.exists('ds/modelRes.pkl'):
+    with open('ds/modelRes.pkl', 'rb') as f:
+        modelResOld = pickle.load(f)
+        score1, score2 = CompareLogisticModels(modelRes, modelResOld, xResval, yResval)
+        print(score1, score2)
+        if score1 > score2:
+            modelRes = modelResOld
+            with open('ds/modelRes.pkl', 'wb') as j:
+                pickle.dump(modelRes, j)
+        else:
+            print('Old model is better or equal.')
 else:
-    print('Too few entries.')
+    with open('ds/modelRes.pkl', 'wb') as j:
+        pickle.dump(modelRes, j)
+    print('Models created and saved.')
