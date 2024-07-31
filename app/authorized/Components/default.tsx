2c577c70f3ed628c1ab609251d787d3b8e6d8283
JulietaGT <julietaguerson@gmail.com>
2024-07-31 19:41:22 -0300

    pequeños cambios

--- app/authorized/Components/default.tsx
+++ app/authorized/Components/default.tsx
@@ -1,8 +1,11 @@
 export default function Default() {
     return (
         <main>
-            <div>
-                <p>Como Usar nuestro sistema?</p>
+            <div className="w-full h-screen flex flex-col justify-center items-center">
+                <p className="text-xl sm:text-2xl font-bold text-left text-primary_light w-full mb-4 mt-4">Como Usar nuestro sistema?</p>
+                <div className="w-11/12 bg-primary_light h-1/2 rounded-sm p-4">
+                    Demostración de Uso:
+                </div>
             </div>
         </main>
     )
