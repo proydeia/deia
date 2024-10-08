97d2a80ae4a9f891bcb87907099e67a84d4b240d
Julieta G <julietaguerson@gmail.com>
2024-10-08 12:18:48 -0300

    ola

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Mision from "./componenetes/mision";
 import About from "./componenetes/about";
-
+//hola
 export default function Home() {
   return (
     <main>
