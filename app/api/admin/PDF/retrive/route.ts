aa78bd41ba11ccf690e3cc2225b1714348e9d3a5
DEIA <168832915+proydeia@users.noreply.github.com>
2024-10-23 15:41:08 -0300

    perhaps???

--- app/api/admin/PDF/retrive/route.ts
+++ app/api/admin/PDF/retrive/route.ts
@@ -1,21 +1,21 @@
 import { userData } from '@/app/api/auth/userData';
 import { list } from '@vercel/blob';
 
-export async function POST(): Promise<{file:any, aproved:boolean}> {
-  const user = await userData();
-  const { blobs } = await list();
+// export async function POST(): Promise<{file:any, aproved:boolean}> {
+//   const user = await userData();
+//   const { blobs } = await list();
   
-  if(!user) return { file: null, aproved: false };
-  if(!blobs) return { file: null, aproved: false };
+//   if(!user) return { file: null, aproved: false };
+//   if(!blobs) return { file: null, aproved: false };
 
-  const blob = blobs.find((blob) => {
-    if(blob.pathname.includes(`${user.id}.pdf`)) return blob;
-  });
+//   const blob = blobs.find((blob) => {
+//     if(blob.pathname.includes(`${user.id}.pdf`)) return blob;
+//   });
 
-  if(!blob) return { file: null, aproved: false };
+//   if(!blob) return { file: null, aproved: false };
 
-  return {
-    file: blob, 
-    aproved: blob.pathname.includes('aproved')
-  };
-}
+//   return {
+//     file: blob, 
+//     aproved: blob.pathname.includes('aproved')
+//   };
+// }
