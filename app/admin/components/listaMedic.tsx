932b7627108246648023d820072b9889bcfe8871
Julieta G <julietaguerson@gmail.com>
2024-11-05 12:01:48 -0300

    diseño masomenos hecho

--- app/admin/components/listaMedic.tsx
+++ app/admin/components/listaMedic.tsx
@@ -3,20 +3,20 @@ import { getMedic, getMedicList, deleteUser } from "@/app/api/admin/admin";
 
 
 export default async function ListaMedic() {
-    let medicos = await getMedicList();
-    
-    let searchTerm = '';
-    
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        "use client";
-        searchTerm = event.target.value.toLowerCase();
-        medicos = medicos.filter((medico) => medico.name.toLowerCase().includes(searchTerm));
-    };
-    
-    return (
-        <div className="flex flex-row">
-        <div className="pt-24 w-1/2">
-          {/* <div className="flex flex-col gap-1 items-start mx-10 justify-between">
+  let medicos = await getMedicList();
+
+  let searchTerm = '';
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    "use client";
+    searchTerm = event.target.value.toLowerCase();
+    medicos = medicos.filter((medico) => medico.name.toLowerCase().includes(searchTerm));
+  };
+
+  return (
+    <div className="flex flex-row w-5/12">
+      <div className="pt-24 w-full">
+         <div className="flex flex-col gap-1 items-start mx-10 justify-between">
             <h3 className="text-2xl  font-bold  font-subtitulo text-left  mt-4 mb-2">
               Lista Medicos
             </h3>
@@ -24,44 +24,45 @@ export default async function ListaMedic() {
               type="search"
               name="search"
               id="search"
-              value={searchTerm}
-              onChange={handleSearchChange}
+              // value={searchTerm}
+              // onChange={handleSearchChange}
               placeholder="Nombre del Médico"
               className="self-auto block h-10 mr-12 rounded-md border-0 py-2 pl-2 pr-10 text-gray-900 shadow-sm  placeholder:text-gray-400 sm:text-sm sm:leading-6"
             />
-          </div> */}
-          <div className="w-10/12 mx-4 mb-6 mt-3  overflow-y-auto">
-            {medicos.map((medico) => (
-              <div
-                className="mt-4 mb-4 flex flex-col gap-4 shadow-md rounded-xl bg-primary_light"
-                key={medico.id}
-              >
-                <div className="flex flex-row  justify-between items-center rounded-xl">
-                  <form
-                    action={async () => {
-                      "use server";
-                      console.log(await getMedic(medico.id));
-                    }}
-                  >
-                    <button type="submit" className=" rounded-md m-1">
-                      <h3 className="m-1">{medico.name}</h3>
-                    </button>
-                  </form>
-                  <form
-                    action={async () => {
-                      "use server";
-                      await deleteUser(medico.id);
-                    }}
-                  >
-                    <button type="submit" className="bg-red p-1 rounded-md m-1">
-                      <h3 className="m-1"> Eliminar Medico</h3>
-                    </button>
-                  </form>
-                </div>
+          
+        <div className="w-10/12 mx-4 mb-6 mt-3  overflow-y-auto">
+          {medicos.map((medico) => (
+            <div
+              className="mt-4 mb-4 flex flex-col gap-4 shadow-md rounded-xl bg-primary_light"
+              key={medico.id}
+            >
+              <div className="flex flex-row  justify-between items-center rounded-xl">
+                <form
+                  action={async () => {
+                    "use server";
+                    console.log(await getMedic(medico.id));
+                  }}
+                >
+                  <button type="submit" className=" rounded-md m-1">
+                    <h3 className="m-1">{medico.name}</h3>
+                  </button>
+                </form>
+                <form
+                  action={async () => {
+                    "use server";
+                    await deleteUser(medico.id);
+                  }}
+                >
+                  <button type="submit" className="bg-red p-1 rounded-md m-1">
+                    <h3 className="m-1"> Eliminar Medico</h3>
+                  </button>
+                </form>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-        </div>
-    )
+      </div>
+    </div>
+    </div>
+  )
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -9,11 +9,11 @@ import ListaMedic from "./components/listaMedic";
 export default async function adminPage() {
   const { file, aproved } = await retrive();
   return (
-    <main className=" bg-fondo_light h-screen">
-      <h1 className='text-center font-black m-3'>Admin page</h1>
+    <main className="bg-fondo_light h-screen flex flex-row justify-stretch w-screen">
+      {/* <h1 className='text-center font-black m-3'>Admin page</h1> */}
        <ListaMedic/>
-        <div className="w-1/2 bg-primary h-screen mt-12  shadow-2xl relative">
-          <div className="mt-12 ">
+        <div className="w-7/12 rounded-l-3xl bg-primary h-screen mt-16 shadow-2xl relative">
+          <div className="mt-8  ">
             <h3 className="text-2xl font-bold px-6 py-3 font-subtitulo text-left w-full mt-4 mb-2 bg-primary_light ">
               Crear Medicos
             </h3>
