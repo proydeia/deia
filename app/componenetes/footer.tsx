b6016fee2ba8039230923ba0170d9139d7610625
proydeia <proydeia@gmail.com>
2024-11-16 23:49:46 -0300

    wot?

--- app/componenetes/footer.tsx
+++
@@ -1,8 +0,0 @@
-//hola ola
-export default function Footer(){
-    return(
-        <div className="w-full bg-dark px-6 py-4">
-            <p className="font-subtitulo font-normal text-white">Proyecto DEIA 2024</p>
-        </div>
-    )
-}
